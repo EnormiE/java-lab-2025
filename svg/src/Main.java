000c7152ba98660009115befae4c17d7e3a400ae
EnormiE <123473330+EnormiE@users.noreply.github.com>
2025-02-28 09:22:16 +0100

    Zadanie 1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Point p = new Point();
+        p.x = 4.5;
+        p.y = 2.0;
+
+//        System.out.println(p.x + ", " + p.y);
+        System.out.println(p);
     }
 }
---
+++ svg/src/Point.java
@@ -0,0 +1,16 @@
+public class Point {
+    public double x;
+    public double y;
+
+//    public String toString() {
+//        return x + ", " + y;
+//    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
