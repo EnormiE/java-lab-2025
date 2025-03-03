563034ffb072f067b390839d95e7695c856b3003
EnormiE <123473330+EnormiE@users.noreply.github.com>
2025-03-03 13:19:10 +0100

    Zadanie 4

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         p.x = 4.5;
         p.y = 2.0;
 
-        System.out.println(p);
-        System.out.println(p);
+        System.out.println(p + "\n");
         Point p2 = p.translated(2.0, 1.0);
         System.out.println(p + "\n" + p2);
         Segment s1 = new Segment();
-//        s1 =
-        System.out.println(s1);
+        s1.a = p;
+        s1.b = p2;
+        System.out.println(s1.length());
 
     }
 }
