7cf902a97f2ca66921c728dde033ff70d1492088
EnormiE <123473330+EnormiE@users.noreply.github.com>
2025-03-03 15:40:10 +0100

    Zadanie 5 - Main

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -4,13 +4,30 @@ public class Main {
         p.x = 4.5;
         p.y = 2.0;
 
-        System.out.println(p + "\n");
         Point p2 = p.translated(2.0, 1.0);
-        System.out.println(p + "\n" + p2);
+
         Segment s1 = new Segment();
         s1.a = p;
         s1.b = p2;
-        System.out.println(s1.length());
 
+        Point p3 = p2.translated(0.5, 2.0);
+
+        Segment s2 = new Segment();
+        s2.a = p2;
+        s2.b = p3;
+        System.out.println("p: "+ p + "\np2: " + p2 + "\np3: " + p3);
+        Segment[] segments = {s1, s2};
+//        System.out.println("\n" + segments[0].length() + " " + segments[1].length());
+        System.out.println("The longest segment:\n" + longest_segment(segments).a + " - " + longest_segment(segments).b);
+    }
+
+    public static Segment longest_segment(Segment[] segments) {
+        Segment longest = segments[0];
+        for (Segment segment : segments) {
+            if (segment.length() >= longest.length()) {
+                longest = segment;
+            }
+        }
+        return longest;
     }
 }
