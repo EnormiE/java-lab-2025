66a0d84c07ae5d94a698a5d093b706b4844f33da
EnormiE <123473330+EnormiE@users.noreply.github.com>
2025-04-24 21:35:51 +0200

    L04_Z02
    Zadanie 2.
    
    Zdefiniuj klasę ShapeDecorator implementującą interfejs Shape, która posiadać będzie chronione pole Shape decoratedShape. Pole to powinno być ustawiane w konstruktorze. Nadpisz metodę toSvg w taki sposób, by zawierała wywołanie tej samej metody na rzecz obiektu decoratedShape.
    
    Po klasie ShapeDecorator podziedzicz nową klasę SolidFillShapeDecorator. Klasa ta powinna posiadać prywatne pole String color. W konstruktorze ma przyjmować obiekt klasy Shape oraz kolor wypełnienia typu String. W jej metodzie toSvg wywołaj metodę toSvg na rzecz decoratedShape, z parametrami jak w zadaniu 1.
    
    Utwórz dwa obiekty klasy SolidFillShapeDecorator tak, aby parametrem jednego był obiekt wielokąta, a drugiego elipsy.

--- svg_decorator/src/Main.java
+++ svg_decorator/src/Main.java
@@ -2,19 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Polygon triangle = new Polygon(new Vec2[]{
-                new Vec2(0, 0),
-                new Vec2(300, 0),
-                new Vec2(150, 250)
-        });
-
-        Polygon rectangle = new Polygon(new Vec2[]{
-                new Vec2(350, 0),
-                new Vec2(750, 0),
-                new Vec2(750, 200),
-                new Vec2(350, 200)
-        });
-
         Polygon pentagon = new Polygon(new Vec2[]{
                 new Vec2(0, 260),
                 new Vec2(100, 460),
@@ -26,8 +13,6 @@ public class Main {
         Ellipse ellipse = new Ellipse(new Vec2(500, 700), 400, 100);
 
         SvgScene scene = new SvgScene();
-        scene.addShape(triangle);
-        scene.addShape(rectangle);
         scene.addShape(pentagon);
         scene.addShape(ellipse);
         scene.save("result.svg");
