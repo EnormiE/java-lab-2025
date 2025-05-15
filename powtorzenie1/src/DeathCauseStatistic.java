e6cc7c4ae2e805ca6d1d6979831611ab8545d3d3
EnormiE <123473330+EnormiE@users.noreply.github.com>
2025-05-15 17:31:54 +0200

    L09_Z02
    Zadanie 2.
    
    Wewnątrz klasy DeathCauseStatistic zdefiniuj publiczną klasę AgeBracketDeaths posiadającą publiczne, ostateczne pola typu int: young, old, deathCount zawierające granice grupy wiekowej i liczbę zgonów.
    
    W klasie DeathCauseStatistic napisz metodę przyjmującą wiek i zwracającą obiekt AgeBracketDeaths odpowiadający temu wiekowi.

--- powtorzenie1/src/DeathCauseStatistic.java
+++ powtorzenie1/src/DeathCauseStatistic.java
@@ -29,4 +29,40 @@ public class DeathCauseStatistic {
     public String getIcd10code() {
         return icd10code;
     }
+
+    public class AgeBracketDeaths {
+        public final int young;
+        public final int old;
+        public final int deathCount;
+
+        public AgeBracketDeaths(int young, int old, int deathCount) {
+            this.young = young;
+            this.old = old;
+            this.deathCount = deathCount;
+        }
+
+        @Override
+        public String toString() {
+            return "AgeBracketDeaths{" +
+                    "young=" + young +
+                    ", old=" + old +
+                    ", deathCount=" + deathCount +
+                    '}';
+        }
+    }
+
+    public AgeBracketDeaths ageToBrackets(int age) {
+        int[] young = {0, 5, 10, 15, 20 ,25, 30, 35, 40 , 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95};
+        int[] old = {4, 9, 14, 19, 24 ,29, 34, 39, 44 , 49, 54, 59, 64, 69, 74, 79, 84, 89, 94, 999};
+        int i = 0;
+        for (int number : old) {
+            if (age <= number) {
+                return new AgeBracketDeaths(young[i], old[i], deathsByAgeGroup[i]);
+            }
+            else {
+                ++i;
+            }
+        }
+        return null; // gdy ktoś poda wiek > 999
+    }
 }
--- powtorzenie1/src/Main.java
+++ powtorzenie1/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DeathCauseStatistic.fromCsvLine("A02.1          ,5,-,-,-,-,-,-,-,-,-,-,-,-,1,2,-,1,1,-,-,-"));
+        DeathCauseStatistic d1 =  DeathCauseStatistic.fromCsvLine("A02.1          ,5,-,-,-,-,-,-,-,-,-,-,-,-,1,2,-,1,1,-,-,-");
+
+        System.out.println(d1.ageToBrackets(8993));
     }
 }
